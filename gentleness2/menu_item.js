8c89266ea3c95bd72890cd7800a455ede9b38cb8
mambax7 <mambax7@gmail.com>
2015-05-05 09:05:12 -0400

    Original files

---
+++ gentleness2/menu_item.js
@@ -0,0 +1,23 @@
+//サブメニュー 1 のコンテンツ
+var submenu1=new Array()
+submenu1[0]='<a href="#">Test 1</a>'
+submenu1[1]='<a href="#">Test 2</a>'
+submenu1[2]='<a href="#">Test 3</a>'
+submenu1[3]='<a href="#">Test 4</a>'
+
+//サブメニュー 2 のコンテンツ
+var submenu2=new Array()
+submenu2[0]='<a href="#">テスト１</a>'
+submenu2[1]='<a href="#">テスト２</a>'
+submenu2[2]='<a href="#">テスト３</a>'
+		
+//サブメニュー 3 のコンテンツ
+var submenu3=new Array()
+submenu3[0]='<a href="#">てすと１</a>'
+submenu3[1]='<a href="#">てすと２</a>'
+submenu3[2]='<a href="#">テスト３</a>'
+
+var menuwidth='165px' //サブメニューのデフォルト幅
+var menubgcolor='lightyellow'  //サブメニューの背景色
+var disappeardelay=250  //サブメニュー表示時間 (in miliseconds)
+var hidemenu_onclick="yes" //クリックしたらメニューを隠す?
